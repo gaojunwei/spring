facdb50bdf4632b7a9de6d825c0d8b6b286f6388
高军威 <gjw.5@qq.com>
2016-09-23 13:34:55 +0800

    修改

---
+++ WEB-INF/ssz_src/com/fh/controller/base/BaseController.java
@@ -0,0 +1,37 @@
+package com.fh.controller.base;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fh.util.PageData;
+
+public class BaseController {
+
+	/**
+	 * 得到PageData
+	 */
+	public PageData getPageData(){
+		return new PageData(this.getRequest());
+	}
+	
+	/**
+	 * 得到ModelAndView
+	 */
+	public ModelAndView getModelAndView(){
+		return new ModelAndView();
+	}
+	
+	/**
+	 * 得到request对象
+	 */
+	public HttpServletRequest getRequest() {
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		
+		return request;
+	}
+	
+}
